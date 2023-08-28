963c44fc5ea66fe5daf9d5ac50a5df641f126f7e
Andrew Wei <andrewwei007@gmail.com>
2023-08-28 14:22:19 -0400

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Andrew Wei on 2023-08-28.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
