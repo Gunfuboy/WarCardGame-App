2f810c94a0d48b6a281a50d1c11d7e1a55a5ec84
Andrew Wei <andrewwei007@gmail.com>
2023-08-29 14:31:23 -0400

    feat: UI for frontpage

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("logo")
+                    .padding(.bottom, 20.0)
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                .padding(.vertical, 20.0)
+                Image("button")
+                    .padding(20.0)
+                HStack{
+                    Spacer()
+                    VStack(alignment: .center){
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .padding(20.0)
+                .foregroundColor(Color.white)
+            }
         }
-        .padding()
+
     }
 }
 
